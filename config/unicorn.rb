505f3a07b5725c0dd5b36950dfa582ea5a7bbb78
Ken Erickson <bookworm.productions@gmail.com>
2011-07-28 09:06:35 -0700

    misc fixes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,16 +2,16 @@
 worker_processes 2 
 
 # Help ensure your application will always spawn in the symlinked "current" directory that Capistrano sets up
-working_directory "/home/root/kerickson.me/me" 
+working_directory "/home/root/vault.kerickson.me/idea_vault" 
 
 # Listen on a Unix domain socket, use the default backlog size
-listen "/tmp/me.sock", :backlog => 1024
+listen "/tmp/vault.sock", :backlog => 1024
 
 # Nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
 # Lets keep our process id's in one place for simplicity  
-pid "/home/root/kerickson.me/me/pids/unicorn.pid"
+pid "/home/root/vault.kerickson.me/idea_vault/pids/unicorn.pid"
 
 # Logs are very useful for trouble shooting, use them 
 stderr_path "/var/log/unicorn/stderr.log"
