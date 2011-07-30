48a5676d99d3ddd22cf5b2b6eeb0676e587fde4f
Ken Erickson <bookworm.productions@gmail.com>
2011-07-30 09:16:52 -0700

    file uploader

---
+++ lib/file_uploader.rb
@@ -0,0 +1,40 @@
+class CoverUploader < CarrierWave::Uploader::Base            
+  include CarrierWave::RMagick
+
+  ##
+  # Storage type
+  #   
+  storage :file   
+  storage :s3 if Padrino.env == :production   
+   
+  configure do |config|         
+    config.s3_cnamed = true
+    config.s3_access_key_id     = ENV['S3_ACCESS_KEY']
+    config.s3_secret_access_key = ENV['S3_SECRET_ACCESS_KEY']
+    config.s3_bucket =  ENV['S3_BUCKET']          
+  cnd
+  
+  ## Manually set root
+  def root; File.join(Padrino.root,"public/"); end
+
+  ##
+  # Directory where uploaded files will be stored (default is /public/uploads)
+  # 
+  def store_dir
+    'uploads/files' if Padrino.env == :development
+    'files' if Padrino.env == :production  
+  end
+
+  ##
+  # Directory where uploaded temp files will be stored (default is [root]/tmp)
+  # 
+  def cache_dir
+    Padrino.root("tmp")
+  end
+
+	def url
+		string = ''
+		string << 'http://' << s3_bucket if Padrino.env == :production  
+		string << '/' << store_dir << '/' << version_name.to_s << '_' << filename 
+	end
+end
